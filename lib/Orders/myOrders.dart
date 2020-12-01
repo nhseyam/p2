3e42004e7f47c8c9e3a286bf07e13b01ec610fb4
nhseyam <nahidhasanseyam@gmail.com>
2020-12-01 08:35:53 +0600

    initial commit

--- lib/Orders/myOrders.dart
+++ lib/Orders/myOrders.dart
@@ -10,13 +10,72 @@ class MyOrders extends StatefulWidget {
   _MyOrdersState createState() => _MyOrdersState();
 }
 
-
-
 class _MyOrdersState extends State<MyOrders> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(),
+      child: Scaffold(
+        appBar: AppBar(
+          iconTheme: IconThemeData(color: Colors.white),
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Colors.green[100], Colors.red],
+                  end: const FractionalOffset(0.0, 0.0),
+                  begin: const FractionalOffset(1.0, 0.0),
+                  stops: [0.0, 1.0],
+                  tileMode: TileMode.clamp),
+            ),
+          ),
+          centerTitle: true,
+          title: Text("My Order"),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.arrow_drop_down_circle_outlined),
+              onPressed: () {
+                SystemNavigator.pop();
+              },
+            )
+          ],
+        ),
+        body: StreamBuilder<QuerySnapshot>(
+          stream: EcommerceApp.firestore
+              .collection(EcommerceApp.collectionUser)
+              .document(EcommerceApp.sharedPreferences
+                  .getString(EcommerceApp.userUID))
+              .collection(EcommerceApp.collectionOrders)
+              .snapshots(),
+          builder: (c, snapshot) {
+            return snapshot.hasData
+                ? ListView.builder(
+                    itemCount: snapshot.data.documents.length,
+                    itemBuilder: (c, index) {
+                      return FutureBuilder<QuerySnapshot>(
+                        future: Firestore.instance
+                            .collection("Items")
+                            .where("shortInfo",
+                                whereIn: snapshot.data.documents[index]
+                                    .data[EcommerceApp.productID])
+                            .getDocuments(),
+                        builder: (c, snap) {
+                          return snap.hasData
+                              ? OrderCard(
+                                  itemCount: snapshot.data.documents.length,
+                                  data: snap.data.documents,
+                                  orderID:
+                                      snapshot.data.documents[index].documentID,
+                                )
+                              : Center(child: circularProgress());
+                        },
+                      );
+                    },
+                  )
+                : Center(
+                    child: circularProgress(),
+                  );
+          },
+        ),
+      ),
     );
   }
 }
--- lib/Widgets/orderCard.dart
+++ lib/Widgets/orderCard.dart
@@ -1,25 +1,140 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:e_shop/Admin/adminOrderCard.dart';
 import 'package:e_shop/Orders/OrderDetailsPage.dart';
 import 'package:e_shop/Models/item.dart';
 import 'package:flutter/material.dart';
 import '../Store/storehome.dart';
 
+int counter = 0;
 
 class OrderCard extends StatelessWidget {
+  final int itemCount;
+  final List<DocumentSnapshot> data;
+  final String orderID;
+  OrderCard({Key key, this.itemCount, this.data, this.orderID})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  InkWell(
+    return InkWell(
+      onTap: () {
+        Route route;
+        if (counter == 0) {
+          counter = counter + 1;
+          route =
+              MaterialPageRoute(builder: (c) => OrderDetails(orderID: orderID));
+        }
+        Navigator.push(context, route);
+      },
+      child: Container(
+        color: Colors.grey[300],
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.all(10),
+        height: itemCount * 145.0,
+        child: ListView.builder(
+          itemCount: itemCount,
+          physics: NeverScrollableScrollPhysics(),
+          itemBuilder: (c, index) {
+            ItemModel model = ItemModel.fromJson(data[index].data);
+            return sourceOrderInfo(model, context);
+          },
+        ),
+      ),
     );
   }
 }
 
+Widget sourceOrderInfo(ItemModel model, BuildContext context,
+    {Color background}) {
+  width = MediaQuery.of(context).size.width;
 
-
-Widget sourceInfo(ItemModel model, BuildContext context,
-    {Color background})
-{
-  width =  MediaQuery.of(context).size.width;
-
-  return  Container();
+  return Expanded(
+    child: Container(
+      color: Colors.grey[100],
+      height: 140,
+      width: width,
+      child: Row(
+        children: [
+          Image.network(
+            model.thumbnailUrl,
+            width: 140,
+            height: 140,
+          ),
+          SizedBox(width: 10),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 5),
+                Container(
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Expanded(
+                        child: Text(
+                          model.title,
+                          style: TextStyle(color: Colors.black, fontSize: 16),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 5),
+                Container(
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Expanded(
+                        child: Text(
+                          model.shortInfo,
+                          style: TextStyle(color: Colors.black45, fontSize: 8),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    SizedBox(width: 5),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(top: 5),
+                          child: Row(
+                            children: [
+                              AutoSizeText(
+                                r"Total Price: ",
+                                style:
+                                    TextStyle(fontSize: 14, color: Colors.grey),
+                                maxLines: 1,
+                              ),
+                              AutoSizeText(
+                                r'$',
+                                style: TextStyle(
+                                    fontSize: 15, color: Colors.green),
+                                maxLines: 1,
+                              ),
+                              AutoSizeText(
+                                (model.price).toString(),
+                                style: TextStyle(
+                                    fontSize: 15, color: Colors.green),
+                                maxLines: 1,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    ),
+  );
 }
